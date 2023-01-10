510db29998fc46ec4e87153bed421cb7be6ed0aa
Nathan Krasney <natankrasney@gmail.com>
2023-01-10 11:26:14 +0200

    Add MuiResponsive

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <div>
       <MuiColorTabs/>
+      <h3>change the viewport width to see responsive change</h3>
       <MuiResponsive/>
     </div>
   )
--- src/components/MuiResponsive.tsx
+++ src/components/MuiResponsive.tsx
@@ -7,7 +7,13 @@ const MuiResponsive = () => {
       sx={{
         marginTop: "1rem",
         height: "10rem",
-        width: { xs: "20rem", sm: "30rem", md: "40 rem", lg: "50rem" },
+        width: {
+          xs: "20rem", // apply when viewport is > 0 
+          sm: "30rem", // apply when viewport is >= 600px
+          md: "40rem",// apply when viewport is >= 900px
+          lg: "50rem", // apply when viewport is >= 1200px
+          xl: "60rem", // apply when viewport is >= 1536px
+        },
         bgcolor: "primary.main",
       }}
     />
